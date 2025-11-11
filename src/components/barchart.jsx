138e810bfefb2121c1098620dcefd6420d688943
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-11-11 11:46:59 +0100

    update tags

--- src/components/barchart.jsx
+++ src/components/barchart.jsx
@@ -1,6 +1,27 @@
+import { Bar }  from 'react-chartjs-2';
+import { Chart, BarController, BarElement } from 'chart.js/auto';
+import data from '../data/data.json';
+
 function Barchart () {
+
+    
     return(
-        <h2>Barchart</h2>
+        <section>
+            <h1>Spending last 7 days</h1>
+            {/* <Bar data={expensesData} /> */}
+            <div>
+                <hr />
+                <p>
+                    <span>Total this month</span>
+                    <span>$478.33</span>
+                </p>
+
+                <p>
+                    <span>+2.4%</span>
+                    <span>from last month</span>
+                </p>
+            </div>
+        </section>
     )
 }
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,14 @@
 function Header() {
     return(
-        <h2>Heading</h2>
+        <section className="flex">
+            <div>
+                <p>My Balance</p>
+                <p>$921.48</p>
+            </div>
+            <div className="flex">
+                <img src="/images/logo.svg" alt="logo" />
+            </div>
+        </section>
     )
 }
 
